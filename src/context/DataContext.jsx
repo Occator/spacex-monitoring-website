0cb86ba14c012b6b87309bae4e96461abad3442e
Occator <stban@posteo.de>
2023-10-06 12:27:38 +0200

    added useContext to make data, e.g. mission details, rockets info, launches etc. , app wide available

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+
+export const DataContext = createContext();
+
+export default function DataContextProvider(props) {
+  const [missionsDataContext, setMissionsDataContext] = useState([]);
+  const [launchesDataContext, setLaunchesDataContext] = useState([]);
+  const [rocketsDataContext, setRocketsDataContext] = useState([]);
+  const [launchpadsDataContext, setLaunchpadsDataContext] = useState([]);
+
+  const missionsURL = "https://api.spacexdata.com/v4/history";
+  const launchesURL = "https://api.spacexdata.com/v5/launches";
+  const rocketsURL = "https://api.spacexdata.com/v4/rockets";
+  const launchpadsURL = "https://api.spacexdata.com/v4/launchpads";
+
+  const fetchHelperFunction = async (url) => {
+    try {
+      const res = await fetch(url, {
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      switch (url) {
+        case missionsURL:
+          setMissionsDataContext(data);
+          break;
+        case launchesURL:
+          setLaunchesDataContext(data);
+          break;
+        case rocketsURL:
+          setRocketsDataContext(data);
+          break;
+        case launchpadsURL:
+          setLaunchpadsDataContext(data);
+          break;
+        default:
+          return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchHelperFunction(missionsURL);
+    fetchHelperFunction(rocketsURL);
+  }, []);
+
+  return (
+    <DataContext.Provider
+      value={{
+        missionsDataContext,
+        launchesDataContext,
+        rocketsDataContext,
+        launchpadsDataContext,
+      }}
+    >
+      {props.children}
+    </DataContext.Provider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 import App from "./App";
+import DataContext from "./context/DataContext.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <DataContext>
+      <App />
+    </DataContext>
   </React.StrictMode>
 );
